7f524175dc9f1a2c76178fec41b91481a43f9881
Colin Elsinga <colinelsinga@Colins-MBP-2.router.taotronics.com>
2021-01-20 15:08:11 -0800

    add appointment views

--- stories/index.js
+++ stories/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
+import { action, actions } from "@storybook/addon-actions";
 
 import "index.scss";
 
@@ -11,6 +11,12 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -73,6 +79,14 @@ const interviewer = {
       name: "Sylvia Palmer",
       avatar: "https://i.imgur.com/LpaY82x.png"
 };
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
     
 storiesOf("InterviewerListItem", module)
       .addParameters({
@@ -102,14 +116,6 @@ storiesOf("InterviewerListItem", module)
         />
 ));
 
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
-
 storiesOf("InterviewerList", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -133,6 +139,15 @@ storiesOf("Appointment", module)
   backgrounds: [{ name: "white", value: "#fff", default: true }]
 })
 .add("Appointment", () => <Appointment />)
-.add("Appointment with Time", () => <Appointment time="12pm" />)
 .add("Header", () => <Header time="12pm" />)
-.add("Empty", () => <Empty onAdd={action("onAdd")} />);
+.add("Empty", () => <Empty onAdd={action("onAdd")} />)
+.add("Show", () => <Show
+student="Lydia Miller-Jones" 
+onEdit={action("onEdit")}
+onDelete={action("onDelete")}
+interviewer={interviewer} />)
+.add("Confirm", () => <Confirm  message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
+.add("Status", () => <Status message="Deleting"/>)
+.add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />);
+
+// .add("Appointment with Time", () => <Appointment time="12pm" />)
