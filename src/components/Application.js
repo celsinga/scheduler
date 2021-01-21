b7df9cd215806793747b6510c1a72a9706e6ae26
Colin Elsinga <colinelsinga@Colins-MBP-2.router.taotronics.com>
2021-01-20 22:01:37 -0800

    tues work complete

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,8 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 
+import Appointment from "components/Appointment";
+
 const days = [
   {
     id: 1,
@@ -22,8 +24,81 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 2,
+    time: "2pm",
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "D'Angelo Scott",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "4pm",
+  },
+  {
+    id: 4,
+    time: "5pm",
+    interview: {
+      student: "Elon Musk",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "6pm",
+  },
+  {
+    id: 5,
+    time: "7pm",
+    interview: {
+      student: "Sade",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcom",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  }
+];
+
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  const mappedAppointments = appointments.map((appointment) => {
+    return <Appointment key={appointment.id} {...appointment} />
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -43,7 +118,7 @@ export default function Application(props) {
     />
     </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {mappedAppointments} <Appointment key="last" time="8pm" />
       </section>
     </main>
   );
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from 'react'
 
 import { storiesOf } from "@storybook/react";
 import { action, actions } from "@storybook/addon-actions";
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -148,6 +149,26 @@ onDelete={action("onDelete")}
 interviewer={interviewer} />)
 .add("Confirm", () => <Confirm  message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
 .add("Status", () => <Status message="Deleting"/>)
-.add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />);
+.add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
+.add("Edit Form", () => <Form name="Sylvia" interviewers={interviewers} interviewer="1" onSave={action("onSave")} onCancel={action("onCancel")} />)
+.add("Create Form", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+.add("Appointment Empty", () => (
+  <Fragment>
+    <Appointment id={1} time="12pm" />
+    <Appointment id="last" time="1pm" />
+  </Fragment>
+))
+.add("Appointment Booked", () => (
+  <Fragment>
+    <Appointment
+      id={1}
+      time="12pm"
+      interview={{ student: "Lydia Miller-Jones", interviewer }}
+    />
+    <Appointment id="last" time="1pm" />
+  </Fragment>
+));
+
+
 
 // .add("Appointment with Time", () => <Appointment time="12pm" />)
