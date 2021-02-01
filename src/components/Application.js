6079f9eb22d2d504d0482e3a092076434cecd4ed
Colin Elsinga <colinelsinga@Colins-MBP-2.router.taotronics.com>
2021-01-31 17:04:45 -0800

    fix functionality req where user could make appointment without an interviewer selected

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-import axios from "axios";
 
 import "components/Application.scss";
 
@@ -16,7 +15,6 @@ import useApplicationData from "hooks/useApplicationData";
 export default function Application(props) {
   const {
     state,
-    setState,
     setDay,
     bookInterview,
     cancelInterview
@@ -33,24 +31,6 @@ export default function Application(props) {
       <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={interview} interviewers={interviewerForDay} bookInterview={bookInterview} cancelInterview={cancelInterview}/> 
     )
   });
-
-  useEffect(() => {
-    const daysURL = axios.get(`/api/days`);
-    const apptURL = axios.get(`/api/appointments`);
-    const interURL = axios.get(`/api/interviewers`);
-    Promise.all([
-      Promise.resolve(daysURL),
-      Promise.resolve(apptURL),
-      Promise.resolve(interURL)
-    ]).then((all) => {
-      setState({
-        ...state,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data
-      })
-    })
-  }, []);
 
   return (
     <main className="layout">
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import axios from "axios";
 
@@ -58,6 +58,23 @@ export default function useApplicationData(props) {
       })
     })
   }
+  useEffect(() => {
+    const daysURL = axios.get(`/api/days`);
+    const apptURL = axios.get(`/api/appointments`);
+    const interURL = axios.get(`/api/interviewers`);
+    Promise.all([
+      Promise.resolve(daysURL),
+      Promise.resolve(apptURL),
+      Promise.resolve(interURL)
+    ]).then((all) => {
+      setState({
+        ...state,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      })
+    })
+  }, []);
 
   return { state, setState, setDay, bookInterview, cancelInterview }
 }
